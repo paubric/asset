0c73993b0b97f0c0c0f68c367e4dc9d33607d3f4
Sturza Mihai <sturzamihai23@gmail.com>
2018-08-28 16:47:07 +0300

    Added BridgeClient
    Added some useful comments.

--- BridgeClient/BridgeClient.ino
+++ BridgeClient/BridgeClient.ino
@@ -1,31 +1,35 @@
+// libraries
 #include <Bridge.h>
 #include <BridgeServer.h>
 #include <BridgeClient.h>
 
+// creating server object
 BridgeServer server;
 
 void setup() {
-  pinMode(13, OUTPUT);
-  
+  // starting Bridge
   Bridge.begin();
-  
+
+  // listening on localhost
   server.listenOnLocalhost();
   server.begin();
 }
 
 void loop() {
+  // accepting connections
   BridgeClient client = server.accept();
 
   if (client) {
     process(client);
     client.stop();
   }
-   delay(50);
 }
 
 void process(BridgeClient client) {
+  // getting url input
   String command = client.readStringUntil('/');
-  
+
+  // validating input
   if(command == "analog") {
     analogCommand(client);
   } else if(command == "digital") {
@@ -41,7 +45,6 @@ void digitalCommand(BridgeClient client) {
 
   pin = client.parseInt();
 
-
   if (client.read() == '/') {
     value = client.parseInt();
     digitalWrite(pin, value);
@@ -49,11 +52,12 @@ void digitalCommand(BridgeClient client) {
     value = digitalRead(pin);
   }
 
-   client.print(F("{\"pin\":"));
-   client.print(pin);
-   client.print(F(",\"value\":"));
-   client.print(value);
-   client.print(F("}"));
+  // printing in a json friendly format
+  client.print(F("{\"pin\":"));
+  client.print(pin);
+  client.print(F(",\"value\":"));
+  client.print(value);
+  client.print(F("}"));
 
   String key = "D";
   key += pin;
@@ -67,8 +71,11 @@ void analogCommand(BridgeClient client) {
 
   if (client.read() == '/') {
     value = client.parseInt();
-    analogWrite(pin, value);
     
+    // setting analog value
+    analogWrite(pin, value);
+
+    // printing in a json friendly format
     client.print(F("{\"pin\":"));
     client.print(pin);
     client.print(F(",\"value\":"));
@@ -79,8 +86,10 @@ void analogCommand(BridgeClient client) {
     key += pin;
     Bridge.put(key, String(value));
   } else {
+    // reading analog value
     value = analogRead(pin);
 
+    // printing in a json friendly format
     client.print(F("{\"pin\":"));
     client.print(pin);
     client.print(F(",\"value\":"));
@@ -97,8 +106,10 @@ void modeCommand(BridgeClient client) {
   int pin;
 
   pin = client.parseInt();
-  
+
+  // encountered an error
   if (client.read() != '/') {
+    // printing in a json friendly format
     client.print(F("{\"pin\":"));
     client.print(pin);
     client.println(F(",\"value\":\"error\"}"));
@@ -107,22 +118,27 @@ void modeCommand(BridgeClient client) {
 
   String mode = client.readStringUntil('\r');
 
+  // setting to input mode
   if (mode == "input") {
     pinMode(pin, INPUT);
+    // printing in a json friendly format
     client.print(F("{\"pin\":"));
     client.print(pin);
     client.print(F(",\"value\":\"input\"}"));
     return;
   }
 
+  // setting to output mode
   if (mode == "output") {
     pinMode(pin, OUTPUT);
+    // printing in a json friendly format
     client.print(F("{\"pin\":"));
     client.print(pin);
     client.print(F(",\"value\":\"output\"}"));
     return;
   }
-
+  // encountered invalid mode
+    // printing in a json friendly format
     client.print(F("{\"pin\":"));
     client.print(pin);
     client.println(F(",\"value\":\"invalid\"}"));
