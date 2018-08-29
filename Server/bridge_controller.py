839b9658f87cc92dbd06f2ff26b6506cf78856a4
paubric <paubric@gmail.com>
2018-08-29 15:10:13 +0300

    Update bridge_controller.py bridge_test.py

--- Server/bridge_controller.py
+++ Server/bridge_controller.py
@@ -1,32 +1,38 @@
 import requests
 
-name_map = {
-    'RTU1': 'yun-mihai',
-    'RTU2': 'arduinoYUN'
-}
+def read_all(device):
+    r = requests.get('http://'+device+'.local'+'/arduino')
+    return r.json()
 
-def digital_write(device, pin, value):
-    r = requests.get('http://'+name_map[device]+'.local'+'/arduino'+'/digital/'+str(pin)+'/'+str(value))
+def digital_read_all(device):
+    r = requests.get('http://'+device+'.local'+'/arduino'+'/digital')
+    return r.json()
+
+def digital_read_pin(device, pin):
+    r = requests.get('http://'+device+'.local'+'/arduino'+'/digital/'+str(pin))
+    return r.json()
+
+def digital_write_pin(device, pin, value):
+    r = requests.get('http://'+device+'.local'+'/arduino'+'/digital/'+str(pin)+'/'+str(value))
     print(r.text)
     return r.json()
 
-def digital_read(device, pin):
-    r = requests.get('http://'+name_map[device]+'.local'+'/arduino'+'/digital/'+str(pin))
+def analog_read_all(device):
+    r = requests.get('http://'+device+'.local'+'/arduino'+'/analog/')
     return r.json()
 
-def analog_write(device, pin, value):
-    r = requests.get('http://'+name_map[device]+'.local'+'/arduino'+'/analog/'+str(pin)+'/'+str(value))
+def analog_read_pin(device, pin):
+    r = requests.get('http://'+device+'.local'+'/arduino'+'/analog/'+str(pin))
     return r.json()
 
-def analog_read(device, pin):
-    r = requests.get('http://'+name_map[device]+'.local'+'/arduino'+'/analog/'+str(pin))
+def analog_write_pin(device, pin, value):
+    r = requests.get('http://'+device+'.local'+'/arduino'+'/analog/'+str(pin)+'/'+str(value))
+    return r.json()
+
+def send_pulse(device, pin, value):
+    r = requests.get('http://'+device+'.local'+'/arduino'+'/pulse/'+str(pin)+'/'+str(value))
     return r.json()
 
 def set_pin(device, pin, value):
-    r = requests.get('http://'+name_map[device]+'.local'+'/arduino'+'/mode/'+str(pin)+'/'+str(value))
+    r = requests.get('http://'+device+'.local'+'/arduino'+'/mode/'+str(pin)+'/'+str(value))
     return r.json()
-
-def send_pulse(device, pin):
-    r = digital_write(device, pin, 1)
-    r = digital_write(device, pin, 0)
-    return r
--- Server/bridge_test.py
+++ Server/bridge_test.py
@@ -1,7 +1,14 @@
 import bridge_controller as bc
 import time
 
-start = time.time()
-bc.digital_write('RTU1', 13, 1)
-end = time.time()
-print(end-start)
+device = 'RTUa'
+
+bc.set_pin(device, pin, value)
+bc.read_all(device)
+bc.digital_read_all(device)
+bc.digital_read_pin(device, pin)
+bc.digital_write_pin(device, pin, value)
+bc.analog_read_all(device)
+bc.analog_read_pin(device, pin)
+bc.analog_write_pin(device, pin, value)
+bc.send_pulse(device, pin, value)
